f1df9802b13b64c0595e8a1cddc63ef619e67b4b
Adeshina Adenuga <nuga0909@gmail.com>
2022-12-09 12:32:09 +0100

    Fix linter errors for js files

--- src/modules/checkbox.js
+++ src/modules/checkbox.js
@@ -1,9 +1,9 @@
 const completedTrue = (arr, id) => {
-    arr[id].completed = true;
-  };
-  
-  const completedFalse = (arr, id) => {
-    arr[id].completed = false;
-  };
-  
-  export { completedTrue, completedFalse };
+  arr[id].completed = true;
+};
+
+const completedFalse = (arr, id) => {
+  arr[id].completed = false;
+};
+
+export { completedTrue, completedFalse };
